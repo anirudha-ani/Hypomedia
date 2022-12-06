2b084122b96807a6b05e88880441803522cefdec
trajnishBrown <tarun_rajnish@brown.edu>
2022-12-06 18:13:09 -0500

    trnascription

--- client/src/components/Modals/RecordAudioModal/RecordAudioModal.tsx
+++ client/src/components/Modals/RecordAudioModal/RecordAudioModal.tsx
@@ -29,6 +29,10 @@ import { initializeApp } from 'firebase/app'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import FileSaver from 'file-saver'
 import { firebaseConfig } from '../../../global/globalUtils'
+// const speech = require('@google-cloud/speech')
+// import speech from '@google-cloud/speech'
+// import { SpeechClient } from '@google-cloud/speech'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import './RecordAudioModal.scss'
 
 export interface IRecordNodeModalProps {
@@ -72,6 +76,10 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
   const app = initializeApp(firebaseConfig)
   const storage = getStorage()
 
+  // speech to text client
+  // const client = new speech.SpeechClient()
+  // const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
   // deconstruct props variables
   const { isOpen, onClose } = props
 
@@ -80,12 +88,59 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
     onClose()
   }
 
+  // const transcribeContextClasses = async (storageURI: string) => {
+  //   console.log('Transcribing - storage URI: ' + storageURI)
+  //   const audio = {
+  //     uri: storageURI,
+  //   }
+
+  //   const speechContext = {
+  //     phrases: ['$TIME'],
+  //   }
+
+  //   const config = {
+  //     encoding: 'LINEAR16',
+  //     sampleRateHertz: 8000,
+  //     languageCode: 'en-US',
+  //     speechContexts: [speechContext],
+  //   }
+
+  //   const request = {
+  //     config: config,
+  //     audio: audio,
+  //   }
+
+  //   // Detects speech in the audio file.
+  //   const [response] = await client.recognize(request)
+  //   response.results.forEach((result: any, index: any) => {
+  //     const transcript = result.alternatives[0].transcript
+  //     console.log('-'.repeat(20))
+  //     console.log(`First alternative of result ${index}`)
+  //     console.log(`Transcript: ${transcript}`)
+  //   })
+  // }
+
   const recorder = useRecorderPermission('audio')
+
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition()
+
+  if (!browserSupportsSpeechRecognition) {
+    console.log(
+      'Your browser doesn\'t support speech recognition. Please use Google Chrome.'
+    )
+  }
+
   const startRecording = async () => {
     recorder.startRecording()
+    // SpeechRecognition.startListening({ continuous: true })
+    SpeechRecognition.startListening()
   }
+
   const stopRecording = async () => {
     await recorder.stopRecording()
+    SpeechRecognition.stopListening()
+
     const blob = await recorder.getBlob()
     const r = (Math.random() + 1).toString(36).substring(7)
     // invokeSaveAsDialog(blob, 'random_name.webm')
@@ -94,6 +149,7 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
     })
     const storageRef = ref(storage, '/' + currNode.title + '/' + r + '.webm')
     const uploadTask = uploadBytesResumable(storageRef, file)
+
     uploadTask.on(
       'state_changed',
       (snapshot) => {
@@ -107,6 +163,7 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
       () => {
         // download url
         getDownloadURL(uploadTask.snapshot.ref).then(async (url) => {
+          console.log('audio url: ', url)
           const attributes = {
             content: url,
             latitude: 0,
@@ -124,6 +181,13 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
             })
           }
           node && setSelectedNode(node)
+
+          // transcribe speech to text using google speech to text api
+          // POST https://speech.googleapis.com/v1/speech:recognize
+          // https://cloud.google.com/speech-to-text/docs/sync-recognize
+
+          // transcribeContextClasses(url)
+
           onSubmit()
         })
       }
@@ -147,6 +211,7 @@ export const RecordAudioModal = (props: IRecordNodeModalProps) => {
             {/* <audio src={mediaBlobUrl} controls autoPlay loop /> */}
             <Button onClick={startRecording}>Start recording</Button>
             <Button onClick={stopRecording}>Stop recording</Button>
+            <p>{transcript}</p>
           </ModalBody>
         </ModalContent>
       </div>
